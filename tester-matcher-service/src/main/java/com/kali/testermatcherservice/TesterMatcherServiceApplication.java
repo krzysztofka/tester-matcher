906acbbbf71fc67e64ab55cc78b762069f8df95e
kalisiakk <kalisiakk@gmail.com>
2019-12-19 22:59:38 +0100

    Tester matcher BE

---
+++ tester-matcher-service/src/main/java/com/kali/testermatcherservice/TesterMatcherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kali.testermatcherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesterMatcherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesterMatcherServiceApplication.class, args);
+	}
+
+}
---
+++ tester-matcher-service/src/test/java/com/kali/testermatcherservice/TesterMatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kali.testermatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterMatcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
